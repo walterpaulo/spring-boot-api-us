9890e08172577d262baed4ae9a30a878075ba75f
Walter Paulo <walter0paulo@hotmail.com>
2021-09-25 13:36:15 -0300

    Ponto inicial - API REST

---
+++ src/main/java/br/com/uSolution/springbootapius/SpringBootApiUsApplication.java
@@ -0,0 +1,13 @@
+package br.com.uSolution.springbootapius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApiUsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApiUsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/uSolution/springbootapius/SpringBootApiUsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.uSolution.springbootapius;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApiUsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
